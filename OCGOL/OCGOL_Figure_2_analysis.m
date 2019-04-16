26b49d8300c98d217d4d62211be402027b5fda6b
Roland <rzemla@gmail.com>
2019-04-16 16:48:35 -0400

    started figure 2 non-silenced OCGOL analysis

---
+++ OCGOL/OCGOL_Figure_2_analysis.m
@@ -0,0 +1,9 @@
+%% Load in data from each animal
+
+
+%% Generate STCs neurons tuned in either session and plot side by side
+
+
+%% Pie chart of fraction of neurons tuned in each subset (A,B, both, neither)
+
+%% 
