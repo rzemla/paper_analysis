a678486e2ff9d6cc5fac8d9f0845e524c54f9361
Roland <rzemla@gmail.com>
2019-05-16 11:31:47 -0400

    finished writing tuning vector adjust function

--- OCGOL/utilities/transient_rate_in_field.m
+++ OCGOL/utilities/transient_rate_in_field.m
@@ -53,46 +53,19 @@ title('In-field transient rates for all neurons');
 histogram(cell2mat(field_event_rates))
 
 %% Recalculate centroid based on peak with highest transient rate
-rr=3;
+
 %tuning vectors for each ROI
-tun_vectors{rr} = session_vars{1}.Place_cell{4}.Tuning_Specificity.tuning_vector{rr};
+tun_vectors = session_vars{1}.Place_cell{4}.Tuning_Specificity.tuning_vector;
 %original sum vector
-tun_vector{rr} = session_vars{1}.Place_cell{4}.Tuning_Specificity.tuning_vector_specificity(rr);
+tun_vector = session_vars{1}.Place_cell{4}.Tuning_Specificity.tuning_vector_specificity;
 
 %% Turn into function
 %input edges and tuning vector
 %output - adjusted tuning vector
 
-adjust_tuning_vector()
+options.pf.skipDisplay = 0;
+[pf_vector] = adjust_tuning_vector(tun_vectors,tun_vector,placeField_edges,options);
 
-%convert to tuning vector to bin value for each roi
-rad_angles = angle(tun_vectors{rr});
-%convert to degrees
-deg_angles = rad2deg(rad_angles);
-%convert to degree range from 0-360
-neg_angles_logical = deg_angles < 0;
-%add 360 to negative angles
-deg_angles(neg_angles_logical) = 360+deg_angles(neg_angles_logical);
-%convert angles to bin position
-bins = discretize(deg_angles,1:360/100:360);
-
-%check which vectors fall into the place field bin range
-pf_event_keep =  bins >= placeField_edges{rr}(1,1) &   bins <=placeField_edges{rr}(1,2);
-
-%updated vector with adjusted magnitude
-pf_unit_vector_kp = sum(tun_vectors{rr}(pf_event_keep))/abs(sum(tun_vectors{rr}(pf_event_keep)));
-pf_mag_factor_kp = abs(sum(tun_vectors{rr}(pf_event_keep)))/sum(abs(tun_vectors{rr}(pf_event_keep)));
-%update vector
-pf_vector = pf_unit_vector_kp*pf_mag_factor_kp;
-
-%convert angle of vectors to bin position
-figure;
-compass(tun_vectors{rr})
-hold on
-%tuning vectors with multiple fields
-compass(tun_vector{rr},'k');
-%tuning vector based on place field with highest transient rate
-compass(pf_vector,'r');
 
 
 %% Plot rate map, place field edges
