36b11071b1ca02c79da3fd5f82a2f5d315150a8a
Roland <rzemla@gmail.com>
2019-06-21 12:41:39 -0400

    finished visualization of ROIs function

--- OCGOL/utilities/raster_spiral_single_ses.m
+++ OCGOL/utilities/raster_spiral_single_ses.m
@@ -136,16 +136,52 @@ end_idx.B = [diff_B_trials_idx; size(Imaging_split{1}{5}.time_restricted,1)];
 %combined start and end idx
 start_end_idx.B = [start_idx.B, end_idx.B]; 
 
-%% Plot raster, event spiral and matching ROIs from FOV
+%% Get lap indices for each lap in all B or B trials
 
-ROI = 371;
+%get unique lap indices
+lapA_idxs = unique(Behavior_split{1}{4}.resampled.lapNb);
+lapB_idxs = unique(Behavior_split{1}{5}.resampled.lapNb);
+
+%get lap start and end indices for all A or B trials
+%all A
+for ll=1:size(lapA_idxs,1)
+    lap_idxs.A(ll,1) = find(Behavior_split{1}{4}.resampled.lapNb == lapA_idxs(ll),1,'first');
+    lap_idxs.A(ll,2) = find(Behavior_split{1}{4}.resampled.lapNb == lapA_idxs(ll),1,'last');
+end
+
+%all B
+for ll=1:size(lapB_idxs,1)
+    lap_idxs.B(ll,1) = find(Behavior_split{1}{5}.resampled.lapNb == lapB_idxs(ll),1,'first');
+    lap_idxs.B(ll,2) = find(Behavior_split{1}{5}.resampled.lapNb == lapB_idxs(ll),1,'last');
+end
+
+%% Event onsets in run interval
+%for each ROI
+for rr=1:size(events{ss}{1},2)
+    %time of significant run events in A
+    event_norm_time.A{rr} =Imaging_split{1}{4}.time_restricted(find(Event_split{1}{4}.Run.run_onset_binary(:,rr) == 1))/60;
+    %normalizesd position of significant run events in A
+    event_norm_pos_run.A{rr} = Behavior_split{1}{4}.resampled.position_norm(find(Event_split{1}{4}.Run.run_onset_binary(:,rr) == 1));
+    %time of significant run events in B
+    event_norm_time.B{rr} =Imaging_split{1}{5}.time_restricted(find(Event_split{1}{5}.Run.run_onset_binary(:,rr) == 1))/60;
+    %normalizesd position of significant run events in B
+    event_norm_pos_run.B{rr} = Behavior_split{1}{5}.resampled.position_norm(find(Event_split{1}{5}.Run.run_onset_binary(:,rr) == 1));
+end
+
+%% Plot raster, event spiral and matching ROIs from FOV
+%21 partial remap
+%AB_list =[2	5	6	8	11	14	15	16	18	19	21	23	24	30	33	35	36	38	39	41	42	43	46	47	49	50	56	57	58	59	60	61	62	63	64	69	70	71	77	78	80	82	84	86	87	88	91	92	93	95	104	107	110	112	114	115	116	117	118	119	122	125	127	128	130	132	133	134	135	136	137	139	142	143	148	154	155	162	164	165	166	168	169	175	176	177	179	180	181	186	187	188	189	190	191	194	195	197	198	199	201	207	208	213	214	216	217	218	223	226	227	228	231	233	234	235	237	241	242	244	245	246	247	254	256	260	263	264	268	269	270	271	272	274	279	280	282	283	284	285	286	288	291	292	296	298	299	304	306	312	318	320	321	326	329	333	334	343	348	350	357	361	362	363	365	366	367	368	369	370	372	373	374	375	379	380	381	382	384	386	387	389	391	392	393	394	398	399	400	401	402	405	407	408	410	411	412	413	415	416	417	419	423	427	429	431	432	433	436	437	438	440	441	443	447	448	449	450	451	453	457	458	460	461	462	463	466	469	470	471	472	474	475	476	477	478	480	481	482	483	486	489	490	491	492	493	494	495	498	500	506	510	512	513	514	515	519	520	521	522	523	524	525	526	527	529	531	534	535	538	540	541	542	543	544	545	549	554	555	556	557	562	563	564	565	566	568	570	571	574	575	576	578	580	581	583	589	590	591	592	593	594	596	597	598	600	603	604	605	606	607	610	612	614	616	620	621	624	627	629	633	635	639	640	641	642	643	649	651	652	653	655	657	660	665	668	669	672	676	682	687	690	692	701];
+
+%ROI = 38;
 %split cells
 %365,371, 452, 371
-figure
+for ROI=AB_list
+figure('Position',[1920 40 1920 960])
 %Show spatial outline of the component
 subplot(3,5,1)
 imagesc(templates{1}.template);
 hold on
+title(num2str(ROI))
 axes(gca);
 axis square
 xticks(gca,[])
@@ -153,33 +189,45 @@ yticks(gca,[])
 grayMap = brighten(gray,0.5);
 colormap(gca,grayMap)
 %plot componenet outline
-plot(coor_keep_sel{ROI}(1,:),coor_keep_sel{ROI}(2,:),'b', 'LineWidth',1);
+plot(coor_keep_sel{ROI}(1,:),coor_keep_sel{ROI}(2,:),'m', 'LineWidth',1);
 %zoom into component based on center of mass calculation
 xlim([A_keep_sel_com(ROI,2)-30, A_keep_sel_com(ROI,2)+30])
 ylim([A_keep_sel_com(ROI,1)-30, A_keep_sel_com(ROI,1)+30])
 
 %show color-coded (by trial) calcium traces of the component
-subplot(3,5,[2 3])
+subplot(3,5,[2 3 4])
 hold on
 ylim([-0.2 2.5])
+yticks([0 1 2])
 ylabel('dF/F');
-xlabel('Time [min]');
+xticks(0:3:12);
+set(gca,'FontSize',14)
+set(gca,'LineWidth',1)
 %convert to minuntes and offset to start at 0 min (0s)
 %for each split (cell above) % for A trial traces
 for ii=1:size(start_end_idx.A,1) 
 plot(Imaging_split{1}{4}.time_restricted(start_end_idx.A(ii,1):start_end_idx.A(ii,2))/60,...
-    Imaging_split{1}{4}.trace_restricted(start_end_idx.A(ii,1):start_end_idx.A(ii,2),ROI),'b')
+    Imaging_split{1}{4}.trace_restricted(start_end_idx.A(ii,1):start_end_idx.A(ii,2),ROI),'Color',[0 0 1 1],...
+    'LineWidth',1)
 end
 %for each split (cell above) % for B trial traces
 for ii=1:size(start_end_idx.B,1) 
 plot(Imaging_split{1}{5}.time_restricted(start_end_idx.B(ii,1):start_end_idx.B(ii,2))/60,...
-    Imaging_split{1}{5}.trace_restricted(start_end_idx.B(ii,1):start_end_idx.B(ii,2),ROI),'r')
+    Imaging_split{1}{5}.trace_restricted(start_end_idx.B(ii,1):start_end_idx.B(ii,2),ROI),'Color',[1 0 0 1],...
+    'LineWidth',1)
 end
 
 %tuning vectors of the component
-subplot(3,5,4,polaraxes)
-polarplot(x{1},r_scaled{1},'k','Linewidth',1.5)
+pax1 = subplot(3,5,5,polaraxes);
 hold on
+pax1.FontSize = 14;
+angles = 0;
+pax1.ThetaTick = angles;
+thetaticklabels(pax1,{'lap start'});
+rticks(pax1, []);
+rlim(pax1,[0 30]);
+
+polarplot(x{1},r_scaled{1},'k','Linewidth',1.5)
 
 %plot A (2) trial events
 for ll=1:size(idxMin{1}{1},2)
@@ -195,8 +243,50 @@ for ll=1:size(idxMin{1}{2},2)
 end
     
 %spiral map of the componenet
-subplot(3,5,5)
+subplot(3,5,10)
+%correct A
+compass(Place_cell{1}{1}.Tuning_Specificity.tuning_vector{ROI},'b');
+hold on
+%correct B
+compass(Place_cell{1}{2}.Tuning_Specificity.tuning_vector{ROI},'r');
+%deal with labels
+labels = findall(gca,'type','text');
+set(labels,'visible','off');
+%set(findall(gcf, 'String', '30', '-or','String','60') ,'String', '  ');
 
+%only correct
+%only works if using polar axis; plot in cartesian ref frame when using
+%compass
+%polarplot([0+0i,Place_cell{1}{1}.Tuning_Specificity.tuning_vector_specificity(ROI)],'b-','LineWidth',2)
+
+%plot normalized position
+subplot(3,5,[7 8 9])
+hold on
+yticks([0 0.5 1])
+ylabel('Normalized position')
+xlabel('Time [min]');
+xticks(0:3:12);
+ylim([0 1])
+set(gca,'FontSize',14)
+set(gca,'LineWidth',1)
+%A laps
+for ii=1:size(lap_idxs.A,1)
+    plot(Imaging_split{1}{4}.time_restricted(lap_idxs.A(ii,1):lap_idxs.A(ii,2))/60,...
+        Behavior_split{1}{4}.resampled.position_norm(lap_idxs.A(ii,1):lap_idxs.A(ii,2)),...
+        'Color',[0 0 1 0.6],'LineWidth',1.5)
+end
+%B laps
+for ii=1:size(lap_idxs.B,1)
+    plot(Imaging_split{1}{5}.time_restricted(lap_idxs.B(ii,1):lap_idxs.B(ii,2))/60,...
+        Behavior_split{1}{5}.resampled.position_norm(lap_idxs.B(ii,1):lap_idxs.B(ii,2)),...
+        'Color',[1 0 0 0.6],'LineWidth',1.5)
+end
+%overlay significant calcium run events
+%A
+scatter(event_norm_time.A{ROI},event_norm_pos_run.A{ROI},[],[0 0 1],'*')
+%B 
+scatter(event_norm_time.B{ROI},event_norm_pos_run.B{ROI},[],[1 0 0],'*')
+end
 
 %%
 figure('Position',[2600,300,1200,1000]);
