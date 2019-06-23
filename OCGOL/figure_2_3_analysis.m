4d516205218c225244c0928d3c1944e44d020358
Roland <rzemla@gmail.com>
2019-06-22 21:36:59 -0400

    centroid diff export and max bin position export

--- OCGOL/figure_2_3_analysis.m
+++ OCGOL/figure_2_3_analysis.m
@@ -151,7 +151,10 @@ save(fullfile(path_dir{1},'cumul_analysis','centroid.mat'),'centroid_ct');
 %% Calculate centroid difference between A&B tuned neurons (max in field transient rate)
 
 options.tuning_criterion = 'ts';
-centroid_diff_single_ses(session_vars,tunedLogical, pf_vector,field_event_rates,options)
+[cent_diff_AandB] = centroid_diff_single_ses(session_vars,tunedLogical, pf_vector,field_event_rates,options);
+
+%save the fractions output data
+save(fullfile(path_dir{1},'cumul_analysis','centroid_diff.mat'),'cent_diff_AandB');
 
 %% Generate STC maps of neurons tuned in either session and plot side by side
 %customize to add options
