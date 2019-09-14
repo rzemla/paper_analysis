ccf1c81e226cad21259da60e68fefc5a2b7e89a5
Roland <rzemla@gmail.com>
2019-09-14 11:30:58 -0400

    working on rasters for export

--- OCGOL/utilities/plot_raster_spiral_only.m
+++ OCGOL/utilities/plot_raster_spiral_only.m
@@ -21,51 +21,168 @@ Imaging_split{1} = session_vars{1}.Imaging_split;
 Place_cell{1} = session_vars{1}.Place_cell;
 Behavior_split{1} = session_vars{1}.Behavior_split; 
 
+%% Get time limits and other parameters for plotting
+
+%imaging frame-to-frame time interval (period)
+dt = session_vars{1}.Imaging.dt;
+
+%number of laps
+nb_laps = size(session_vars{1}.Behavior.lap,2);
+%
+startT = session_vars{1}.Imaging.time_restricted(1)/60;
+%end seconds
+endT = size(session_vars{1}.Imaging.time_restricted,1)*dt;
+%end minutes
+endT = endT/60;
+
+%axis time limits for visualization
+time_limits = [startT endT];
+%df/f trace axis limits
+dff_limits = [-0.2 4.5];
+
+%red and blue colormaps
+cmap_blue=cbrewer('seq', 'Blues', 64);
+cmap_red=cbrewer('seq', 'Reds', 64);
+
+%set bottom value to red to bottom value of blue
+cmap_red(1,:) = [1 1 1];
+cmap_blue(1,:) = [1 1 1];
+
+%trial order of laps
+trialOrder =session_vars{1}.Behavior.performance.trialOrder;
+%all B lap idxs
+idx_A = find(trialOrder ==2 | trialOrder ==20);
+%all B lap idxs
+idx_B = find(trialOrder ==3 | trialOrder ==30);
+
+%% Plot each dF/F raster in a separate figure window
+
+%define each class of ROI to plot
+ROI_all = [627 416	269 524 458];
+for rr=1:5
+    figure('Position',[2951 443 297 313])
+    %split into 2 and fill with nans areas where opposite trial is there
+    whole_map_temp = session_vars{1}.Place_cell{3}.dF_lap_map_ROI_smooth{ROI_all(rr)};
+    whole_map_temp_A = whole_map_temp;
+    whole_map_temp_B = whole_map_temp;
+    %fill with nans
+    whole_map_temp_A(idx_B,:) = nan;
+    whole_map_temp_B(idx_A,:) = nan;
+
+    %plot A map
+    imAlpha=ones(size(whole_map_temp));
+    imAlpha(isnan(whole_map_temp_A))=0;
+    ax2 = axes();
+    h2 = imagesc(ax2,whole_map_temp_A,'AlphaData',imAlpha);
+    %set colormap to
+    colormap(ax2,cmap_blue);
+    caxis(ax2,[0 2])
+        %plot B map
+    imAlpha=ones(size(whole_map_temp));
+    imAlpha(isnan(whole_map_temp_B))=0;
+    ax3 = axes;
+    h3 = imagesc(ax3,whole_map_temp_B,'AlphaData',imAlpha);
+    colormap(ax3,cmap_red);
+    caxis(ax3,[0 2])
+    ax2.Visible = 'off';
+    ax3.Visible = 'off';
+
+%export each raster
+mkdir(fullfile(crossdir,'match_STC'))
+disp('Saving match ROIs STC ')
+export_fig(f_clip ,fullfile(crossdir,'match_STC','all_matching__nan_d1_clipped_300.png'),'-r300')
+
+end
+
+%make subplots with colorbar figure with colorbar
+figure
+subplot(1,2,1)
+hold on
+    ax1 = gca;
+    colormap(ax1,cmap_blue);
+    caxis(ax1,[0 2])
+    c1 = colorbar(ax1);
+c1.Ticks = [0 0.5 1 1.5 2];
+c1.Label.String = 'dF/F';
+c1.Label.FontSize = 16;
+c1.FontSize = 16;
+axis off
+
+subplot(1,2,2)
+hold on
+    ax2=gca;
+    c2 = colormap(ax2,cmap_red);
+    caxis(ax2,[0 2])
+c2 = colorbar(ax2)
+c2.Ticks = [0 0.5 1 1.5 2];
+c2.Label.String = 'dF/F';
+c2.Label.FontSize = 16;
+c2.FontSize = 16;
+axis off
+
 %% Plot figure 2b traces 
 if options.plotFigure2 ==1
     
+
 %go through each class of ROIs
-for cc=1:size(task_remapping_ROIs.common,2)
+%for cc=1:size(task_remapping_ROIs.global_near,2)
     %common, rate, near
-    %ROI_all = [398	404	455];
-    %
+    %ROI_all = [627	416	440 524 522];
+    %ROI_all = [627	416	269 400 522];
+    %ROI_all = [627	416	381 524 169];
+    %ROI_all = [627	416	381 524 470];
+    ROI_all = [627 416	269 524 458]; % global:78  524 228
+% partial select: 640, 591, 534 (nice), 522 (nice), 519 (nice) , 511, 470 (nice), 458 (nice), 437 (nice), 371, 343,
+% 244 (nice), 233(nice), 206 (nice), 187 (nice), 176 (nice), 169 (nice) , 
+
+for zz=1
     %ROI_all = [562	404	455];
-    ROI_all(1:3) = task_remapping_ROIs.common(cc)
+    %ROI_all(1:3) = task_remapping_ROIs.global_near(cc)
     %ROI_colors = {'b','r','m'};
     %matrix of RGB color values
-    ROI_colors = [65,105,225, 255; 220,20,60, 255; 139, 0, 139, 255]/255;
-    ROI_outline_order = [1,6,11];
-    marker_size = 18;
-    spiral_line_width = 0.7;
-    nb_laps = size(session_vars{1}.Behavior.lap,2);
+    %ROI_colors = [65,105,225, 255; 220,20,60, 255; 139, 0, 139, 255]/255;
+    %all purple
+    ROI_colors = [139, 0, 139, 255; 139, 0, 139, 255; 139, 0, 139, 255; 139, 0, 139, 255; 139, 0, 139, 255]/255;
+    %ROI_outline_order = [1,6,11, 16, 21];
+    ROI_outline_order = 1:6:36;
+    marker_size = 8;
+    spiral_line_width = 0.4;
+    
     figure('Position',[1920 40 1920 960],'Renderer','painters')
     %Show spatial outline of the component
-    for rr=1:3
-        subplot(4,5,ROI_outline_order(rr))
+    for rr=1:5
+        subplot(6,6,ROI_outline_order(rr))
         imagesc(templates{1}.template);
         hold on
+        %set background axis color to black
+        set(gca,'color',0*[1 1 1]);
         title(num2str(ROI_all(rr)))
         axes(gca);
         axis square
         xticks(gca,[])
         yticks(gca,[])
-        
-        grayMap = brighten(gray,0.5);
-        colormap(gca,grayMap)
+        ax = gca;
+        grayMap = brighten(gray,0.4);
+        colormap(ax,grayMap)
         %plot componenet outline
         plot(coor_keep_sel{ROI_all(rr)}(1,:),coor_keep_sel{ROI_all(rr)}(2,:),'Color',ROI_colors(rr,:), 'LineWidth',1);
         %zoom into component based on center of mass calculation
-        xlim([A_keep_sel_com(ROI_all(rr),2)-30, A_keep_sel_com(ROI_all(rr),2)+30])
-        ylim([A_keep_sel_com(ROI_all(rr),1)-30, A_keep_sel_com(ROI_all(rr),1)+30])
+        xlim([A_keep_sel_com(ROI_all(rr),2)-15, A_keep_sel_com(ROI_all(rr),2)+15])
+        ylim([A_keep_sel_com(ROI_all(rr),1)-15, A_keep_sel_com(ROI_all(rr),1)+15])
+hold off
     end
     
     %show color-coded (by trial) calcium traces of the component
-    ROI_trace_order = [2,3,4; 7 8 9; 12 13 14];
-    for rr=1:3
-        subplot(4,5,ROI_trace_order(rr,:))
+    %ROI_trace_order = [2,3,4; 7 8 9; 12 13 14; 17 18 19; 22 23 24];
+    ROI_trace_order(:,1) = 2:6:30;
+    ROI_trace_order(:,2) = 3:6:30;
+    ROI_trace_order(:,3) = 4:6:30;
+
+    for rr=1:5
+        subplot(6,6,ROI_trace_order(rr,:))
         hold on
-        xlim([0.1 11.2])
-        ylim([-0.2 4])
+        xlim(time_limits)
+        ylim(dff_limits)
         yticks([0 1 2])
         ylabel('dF/F');
         xticks(0:3:15.5);
@@ -86,10 +203,11 @@ for cc=1:size(task_remapping_ROIs.common,2)
         end
     end
     
-    ROI_polar_order = [5,10,15];
-    for rr=1:3
+    %ROI_polar_order = [5,10,15,20,25];
+    ROI_polar_order = 5:6:30;
+    for rr=1:5
         %tuning vectors of the component
-        pax1 = subplot(4,5,ROI_polar_order(rr),polaraxes);
+        pax1 = subplot(6,6,ROI_polar_order(rr),polaraxes);
         hold on
         pax1.FontSize = 14;
         angles = 0;
@@ -115,11 +233,68 @@ for cc=1:size(task_remapping_ROIs.common,2)
             %polarscatter(centerB_angle(ii), 20, 'r*','MarkerFaceColor','r');
         end
     end
+%end
+
+%dF/F mean rasters for each ROI
+ROI_dFF_order = 6:6:36;
+for rr=1:5
+    subplot(6,6,ROI_dFF_order(rr))
+    axis off
+    %split into 2 and fill with nans areas where opposite trial is there
+    whole_map_temp = session_vars{1}.Place_cell{3}.dF_lap_map_ROI_smooth{ROI_all(rr)};
+    whole_map_temp_A = whole_map_temp;
+    whole_map_temp_B = whole_map_temp;
+    %fill with nans
+    whole_map_temp_A(idx_B,:) = nan;
+    whole_map_temp_B(idx_A,:) = nan;
+
+    %plot A map
+    imAlpha=ones(size(whole_map_temp));
+    imAlpha(isnan(whole_map_temp_A))=0;
+    ax2 = axes;
+    h2 = imagesc(ax2,whole_map_temp_A,'AlphaData',imAlpha);
+    %set colormap to
+    colormap(ax2,cmap_blue);
+    caxis(ax2,[0 2])
+        %plot B map
+    imAlpha=ones(size(whole_map_temp));
+    imAlpha(isnan(whole_map_temp_B))=0;
+    ax3 = axes;
+    h3 = imagesc(ax3,whole_map_temp_B,'AlphaData',imAlpha);
+    colormap(ax3,cmap_red);
+    caxis(ax3,[0 2])
+    ax2.Visible = 'off';
+    ax3.Visible = 'off';
+
+
 end
 
+%plot normalized position
+subplot(6,6,[32 33 34])
+hold on
+yticks([0 0.5 1])
+ylabel('Normalized position')
+xlabel('Time [min]');
+xticks(0:3:11.2);
+xlim(time_limits)
+ylim([0 1])
+set(gca,'FontSize',14)
+set(gca,'LineWidth',1)
+%A laps
+for ii=1:size(lap_idxs.A,1)
+    plot(Imaging_split{1}{4}.time_restricted(lap_idxs.A(ii,1):lap_idxs.A(ii,2))/60,...
+        Behavior_split{1}{4}.resampled.position_norm(lap_idxs.A(ii,1):lap_idxs.A(ii,2)),...
+        'Color',[0 0 1 0.6],'LineWidth',1.5)
+end
+%B laps
+for ii=1:size(lap_idxs.B,1)
+    plot(Imaging_split{1}{5}.time_restricted(lap_idxs.B(ii,1):lap_idxs.B(ii,2))/60,...
+        Behavior_split{1}{5}.resampled.position_norm(lap_idxs.B(ii,1):lap_idxs.B(ii,2)),...
+        'Color',[1 0 0 0.6],'LineWidth',1.5)
+end
 
-%spiral map of the componenet
-%subplot(4,5,10)
+end
+
 
 %correct A
 % compass(Place_cell{1}{1}.Tuning_Specificity.tuning_vector{ROI},'b');
@@ -136,29 +311,6 @@ end
 %compass
 %polarplot([0+0i,Place_cell{1}{1}.Tuning_Specificity.tuning_vector_specificity(ROI)],'b-','LineWidth',2)
 
-%plot normalized position
-subplot(4,5,[17 18 19])
-hold on
-yticks([0 0.5 1])
-ylabel('Normalized position')
-xlabel('Time [min]');
-xticks(0:3:9.5);
-xlim([0.1 12.2])
-ylim([0 1])
-set(gca,'FontSize',14)
-set(gca,'LineWidth',1)
-%A laps
-for ii=1:size(lap_idxs.A,1)
-    plot(Imaging_split{1}{4}.time_restricted(lap_idxs.A(ii,1):lap_idxs.A(ii,2))/60,...
-        Behavior_split{1}{4}.resampled.position_norm(lap_idxs.A(ii,1):lap_idxs.A(ii,2)),...
-        'Color',[0 0 1 0.6],'LineWidth',1.5)
-end
-%B laps
-for ii=1:size(lap_idxs.B,1)
-    plot(Imaging_split{1}{5}.time_restricted(lap_idxs.B(ii,1):lap_idxs.B(ii,2))/60,...
-        Behavior_split{1}{5}.resampled.position_norm(lap_idxs.B(ii,1):lap_idxs.B(ii,2)),...
-        'Color',[1 0 0 0.6],'LineWidth',1.5)
-end
 
 %overlay significant calcium run events
 %A
