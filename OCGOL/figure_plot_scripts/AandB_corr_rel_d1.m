cb1bd3f4226f71b7fee0d203ff1d4a0c43c8f878
Roland <rzemla@gmail.com>
2019-11-01 20:31:43 -0400

    completed session plots for TC similarity for A&B neurons

--- OCGOL/figure_plot_scripts/AandB_corr_rel_d1.m
+++ OCGOL/figure_plot_scripts/AandB_corr_rel_d1.m
@@ -18,6 +18,11 @@ for aa=1:nb_learn_animals
 end
 
 %% Relative to D1 comparison - LEARN - relative to session only (not sorted by day yet or low trial count/field shift filtered)
+
+%preallocate with nans
+TC_corr_AB_matching_learning_early = nan(nb_learn_animals,max(nb_ses_learn));
+TC_corr_AB_matching_learning_late = nan(nb_learn_animals,max(nb_ses_learn));
+
 %for all learning animals
 for aa=1:nb_learn_animals
     
@@ -86,6 +91,30 @@ for aa=1:nb_learn_animals
         
     end
 end
+
+%% Filter sessions relative to session 1 based on field shift/trial count - LEARN
+%animal 1, session 3,4 - A/B both trials - means
+TC_corr_AB_matching_learning_early(1,[3,4])= nan;
+TC_corr_AB_matching_learning_late(1,[3,4])= nan;
+%each neuron correlation
+TC_corr_AB_matching_learning_early_diag(1,[3,4]) = {nan};
+TC_corr_AB_matching_learning_late_diag(1,[3,4]) = {nan};
+
+%animal 3, session 2 - A/B both trials - means
+TC_corr_AB_matching_learning_early(3,[2])= nan;
+TC_corr_AB_matching_learning_late(3,[2])= nan;
+%each neuron correlation
+TC_corr_AB_matching_learning_early_diag(3,[2]) = {nan};
+TC_corr_AB_matching_learning_late_diag(3,[2]) = {nan};
+
+%animal 5, session 6 and 7 - A/B both trials - means
+TC_corr_AB_matching_learning_early(5,[6,7])= nan;
+TC_corr_AB_matching_learning_late(5,[6,7])= nan;
+%each neuron correlation
+TC_corr_AB_matching_learning_early_diag(5,[6,7]) = {nan};
+TC_corr_AB_matching_learning_late_diag(5,[6,7]) = {nan};
+
+
 
 %% Cross-comparison to all other sessions - LEARN - DEBUG THIS TO WORK ACROSS ALL ANIMALS/SESSION
 for aa=1:nb_learn_animals
@@ -157,6 +186,10 @@ end
 
 %% Relative to D1 comparison - recall
 
+%preallocate with nans
+TC_corr_AB_matching_recall_early = nan(nb_recall_animals,max(nb_ses_recall));
+TC_corr_AB_matching_recall_late = nan(nb_recall_animals,max(nb_ses_recall));
+
 for aa=1:nb_recall_animals
     
     for day_sel=2:7
@@ -224,84 +257,99 @@ for aa=1:nb_recall_animals
     end
 end
 
-%% Get sem for learning and recall for TC correations
+%% Get sem for learning and recall for TC correations -  LEARNING AND RECALL BY ANIMAL AVERAGE
 
-TC_learning_early_sem =nanstd(TC_corr_AB_matching_learning_early,[],1)./sqrt(size(TC_corr_AB_matching_learning_early,1));
-TC_learning_late_sem = nanstd(TC_corr_AB_matching_learning_late,[],1)./sqrt(size(TC_corr_AB_matching_learning_late,1));
+%change this to reflect number of animals per session (by animal)
+nb_animal_per_ses_learn = sum(~isnan(TC_corr_AB_matching_learning_early),1)
+nb_animal_per_ses_recall = sum(~isnan(TC_corr_AB_matching_recall_early),1)
 
-TC_recall_early_sem =nanstd(TC_corr_AB_matching_recall_early,[],1)./sqrt(size(TC_corr_AB_matching_recall_early,1));
-TC_recall_late_sem = nanstd(TC_corr_AB_matching_recall_late,[],1)./sqrt(size(TC_corr_AB_matching_recall_late,1));
+TC_learning_early_sem = nanstd(TC_corr_AB_matching_learning_early,[],1)./sqrt(nb_animal_per_ses_learn);
+TC_learning_late_sem = nanstd(TC_corr_AB_matching_learning_late,[],1)./sqrt(nb_animal_per_ses_learn);
 
+TC_recall_early_sem =nanstd(TC_corr_AB_matching_recall_early,[],1)./sqrt(nb_animal_per_ses_recall);
+TC_recall_late_sem = nanstd(TC_corr_AB_matching_recall_late,[],1)./sqrt(nb_animal_per_ses_recall);
+
+%% Get mean and sem by neuron; not animal -LEARNING
+
+%get total neuron count for sessions - learning nb ROI comparison 
+for ss=2:9
+    %collect all the TC values
+    corr_vals_TC_learn{ss} = cell2mat(TC_corr_AB_matching_learning_early_diag(:,ss));
+    %remove nans
+    corr_vals_TC_learn{ss}(isnan(corr_vals_TC_learn{ss})) = [];
+    %get count
+    nbROI_TC_learn(ss) = size(corr_vals_TC_learn{ss},1);
+end
+
+%get mean across all matching neurons
+for ss=2:9
+    mean_learn_early_all_ROIs(ss) = nanmean(cell2mat(TC_corr_AB_matching_learning_early_diag(:,ss)));
+    mean_learn_late_all_ROIs(ss) = nanmean(cell2mat(TC_corr_AB_matching_learning_late_diag(:,ss)));
+end
+
+%get sem across all matching neurons
+for ss=2:9
+    sem_learn_early_all_ROIs(ss) = nanstd(cell2mat(TC_corr_AB_matching_learning_early_diag(:,ss)),0,1)/sqrt(nbROI_TC_learn(ss));
+    sem_learn_late_all_ROIs(ss) = nanstd(cell2mat(TC_corr_AB_matching_learning_late_diag(:,ss)),0,1)/sqrt(nbROI_TC_learn(ss));
+end
+
+%% Get mean and sem by neuron; not animal -RECALL
+
+%learning nb ROI comparison
+for ss=2:7
+    %collect all the TC values
+    corr_vals_TC_recall{ss} = cell2mat(TC_corr_AB_matching_recall_early_diag(:,ss));
+    %remove nans
+    corr_vals_TC_recall{ss}(isnan(corr_vals_TC_recall{ss})) = [];
+    %get count
+    nbROI_TC_recall(ss) = size(corr_vals_TC_recall{ss},1);
+end
+
+%get mean across all matching neurons
+for ss=2:7
+    mean_recall_early_all_ROIs(ss) = nanmean(cell2mat(TC_corr_AB_matching_recall_early_diag(:,ss)));
+    mean_recall_late_all_ROIs(ss) = nanmean(cell2mat(TC_corr_AB_matching_recall_late_diag(:,ss)));
+end
+
+%get sem across all matching neurons
+for ss=2:7
+    sem_recall_early_all_ROIs(ss) = nanstd(cell2mat(TC_corr_AB_matching_recall_early_diag(:,ss)),0,1)/sqrt(nbROI_TC_recall(ss));
+    sem_recall_late_all_ROIs(ss) = nanstd(cell2mat(TC_corr_AB_matching_recall_late_diag(:,ss)),0,1)/sqrt(nbROI_TC_recall(ss));
+end
 
 %% Construct correlation matrix for learning vs recall - mean of all values
 
 %learning
-for ii=1:6
-    for jj=1:6
-        mean_corr_learn_early(ii,jj) = nanmean(cell2mat(TC_corr_AB_matching_learning_early_diag_all(:,ii,jj)))
-        mean_corr_learn_late(ii,jj) = nanmean(cell2mat(TC_corr_AB_matching_learning_late_diag_all(:,ii,jj)))
-    end
-end
-
-
-figure;
-subplot(2,2,1)
- imagesc(mean_corr_learn_early)
- hold on
- caxis([0.2 0.7]) 
- colormap('jet')
- subplot(2,2,2)
- imagesc(mean_corr_learn_late)
- hold on
- caxis([0.2 0.7]) 
- colormap('jet')
+% for ii=1:6
+%     for jj=1:6
+%         mean_corr_learn_early(ii,jj) = nanmean(cell2mat(TC_corr_AB_matching_learning_early_diag_all(:,ii,jj)))
+%         mean_corr_learn_late(ii,jj) = nanmean(cell2mat(TC_corr_AB_matching_learning_late_diag_all(:,ii,jj)))
+%     end
+% end
+% 
+% 
+% figure;
+% subplot(2,2,1)
+%  imagesc(mean_corr_learn_early)
+%  hold on
+%  caxis([0.2 0.7]) 
+%  colormap('jet')
+%  subplot(2,2,2)
+%  imagesc(mean_corr_learn_late)
+%  hold on
+%  caxis([0.2 0.7]) 
+%  colormap('jet')
  
-%% Do Paired Mann Whitnney U comp on collapsed cells (all neurons)
+%% Statistics - do Paired Mann Whitnney U comp on collapsed cells (all neurons)
 
 %learning
-for test_ses=2:6
-    [p_learn(test_ses),~] = signrank(cell2mat(TC_corr_AB_matching_learning_early_diag(:,test_ses)),cell2mat(TC_corr_AB_matching_learning_late_diag(:,test_ses)));
-end
-
-%learning nb ROi comparison
-for test_ses=2:6
-    nbROI_TC_learn(test_ses) = length(cell2mat(TC_corr_AB_matching_learning_early_diag(:,test_ses)))
+for ss=2:9
+    [p_learn(ss),~] = signrank(cell2mat(TC_corr_AB_matching_learning_early_diag(:,ss)),cell2mat(TC_corr_AB_matching_learning_late_diag(:,ss)));
 end
 
 %recall
-for test_ses = 2:7
-    [p_recall(test_ses),~] = signrank(cell2mat(TC_corr_AB_matching_recall_early_diag(:,test_ses)),cell2mat(TC_corr_AB_matching_recall_late_diag(:,test_ses)));
-end
-
-%learning nb ROI comparison
-for test_ses=2:7
-    nbROI_TC_recall(test_ses) = length(cell2mat(TC_corr_AB_matching_recall_early_diag(:,test_ses)))
-end
-
-%% Get mean and sem by neuron; not animal -LEARNING
-%get mean across all matching neurons
-for test_ses=2:6
-    mean_learn_early_all_ROIs(test_ses) = nanmean(cell2mat(TC_corr_AB_matching_learning_early_diag(:,test_ses)));
-    mean_learn_late_all_ROIs(test_ses) = nanmean(cell2mat(TC_corr_AB_matching_learning_late_diag(:,test_ses)));
-end
-
-%get sem across all matching neurons
-for test_ses=2:6
-    sem_learn_early_all_ROIs(test_ses) = nanstd(cell2mat(TC_corr_AB_matching_learning_early_diag(:,test_ses)),0,1)/sqrt(nbROI_TC_learn(test_ses));
-    sem_learn_late_all_ROIs(test_ses) = nanstd(cell2mat(TC_corr_AB_matching_learning_late_diag(:,test_ses)),0,1)/sqrt(nbROI_TC_learn(test_ses));
-end
-
-%% Get mean and sem by neuron; not animal -RECALL
-%get mean across all matching neurons
-for test_ses=2:7
-    mean_recall_early_all_ROIs(test_ses) = nanmean(cell2mat(TC_corr_AB_matching_recall_early_diag(:,test_ses)));
-    mean_recall_late_all_ROIs(test_ses) = nanmean(cell2mat(TC_corr_AB_matching_recall_late_diag(:,test_ses)));
-end
-
-%get sem across all matching neurons
-for test_ses=2:7
-    sem_recall_early_all_ROIs(test_ses) = nanstd(cell2mat(TC_corr_AB_matching_recall_early_diag(:,test_ses)),0,1)/sqrt(nbROI_TC_recall(test_ses));
-    sem_recall_late_all_ROIs(test_ses) = nanstd(cell2mat(TC_corr_AB_matching_recall_late_diag(:,test_ses)),0,1)/sqrt(nbROI_TC_recall(test_ses));
+for ss = 2:7
+    [p_recall(ss),~] = signrank(cell2mat(TC_corr_AB_matching_recall_early_diag(:,ss)),cell2mat(TC_corr_AB_matching_recall_late_diag(:,ss)));
 end
 
 %%  Plot mean and sem -  neuron mean
@@ -318,8 +366,8 @@ ylim([0.2 0.9])
 p1 = errorbar(mean_learn_early_all_ROIs(:,2:end),sem_learn_early_all_ROIs(2:end),'k-','LineWidth',2);
 %p2 = plot(nanmean(TC_corr_AB_matching_learning_late,1),'k--')
 p2 = errorbar(mean_learn_late_all_ROIs(:,2:end),sem_learn_late_all_ROIs(2:end),'Color', [139,0,139]/255,'LineWidth',2);
-legend([p1 p2], {'Reference','Session'},'Location','southeast')
-xticks([1:5])
+legend([p1 p2], {'Day 1','Session'},'Location','southwest')
+xticks([1:8])
 xtickangle(45)
 for ii=2:6
     x_labels_learn{ii-1} = [num2str(ii)];
@@ -327,7 +375,7 @@ end
 xticklabels(x_labels_learn)
 set(gca,'FontSize',16)
 set(gca,'LineWidth',2)
-xlim([0,6])
+xlim([0,9])
 
 subplot(1,2,2)
 hold on
@@ -340,7 +388,7 @@ ylim([0.2 0.9])
 p1 = errorbar(mean_recall_early_all_ROIs(:,2:end),sem_recall_early_all_ROIs(2:end),'k-','LineWidth',2);
 %p2 = plot(nanmean(TC_corr_AB_matching_learning_late,1),'k--')
 p2 = errorbar(mean_recall_late_all_ROIs(:,2:end),sem_recall_late_all_ROIs(2:end),'Color', [139,0,139]/255,'LineWidth',2);
-legend([p1 p2], {'Reference','Session'},'Location','southeast')
+legend([p1 p2], {'Day 1','Session'},'Location','southeast')
 xticks([1:6])
 xtickangle(45)
 for ii=2:7
@@ -351,7 +399,7 @@ set(gca,'FontSize',16)
 set(gca,'LineWidth',2)
 xlim([0,7])
 
-%% Plot mean and sem - animal mean across all neurons
+%% Plot mean and sem - animal mean (all neurons for given animal matching)
 
 figure('Position',[2058 307 1008 481]);
 subplot(1,2,1)
@@ -359,35 +407,36 @@ hold on
 title('Learning')
 xlabel('Session #')
 ylabel('Correlation score')
-ylim([0.2 0.8])
+ylim([0 0.8])
 %p1 = plot(nanmean(TC_corr_AB_matching_learning_early,1),'k-')
 p1 = errorbar(nanmean(TC_corr_AB_matching_learning_early(:,2:end),1),TC_learning_early_sem(2:end),'k-','LineWidth',2);
 %p2 = plot(nanmean(TC_corr_AB_matching_learning_late,1),'k--')
 p2 = errorbar(nanmean(TC_corr_AB_matching_learning_late(:,2:end),1),TC_learning_late_sem(2:end),'Color', [139,0,139]/255,'LineWidth',2);
-legend([p1 p2], {'Reference','Session'},'Location','southeast')
-xticks([1:5])
+legend([p1 p2], {'Day 1','Session'},'Location','southeast')
+xticks([1:9])
 xtickangle(45)
-for ii=2:6
+for ii=2:9
     x_labels_learn{ii-1} = [num2str(ii)];
 end
 xticklabels(x_labels_learn)
 set(gca,'FontSize',16)
 set(gca,'LineWidth',2)
-xlim([0,6])
+xlim([0,10])
 
 subplot(1,2,2)
 hold on
 title('Recall')
 xlabel('Session #')
 ylabel('Correlation score')
-ylim([0.2 0.8])
+ylim([0 0.8])
 %p1 = plot(nanmean(TC_corr_AB_matching_recall_early,1),'k-')
 p1 = errorbar(nanmean(TC_corr_AB_matching_recall_early(:,2:end),1),TC_recall_early_sem(2:end),'k-','LineWidth',2)
 %p2 = plot(nanmean(TC_corr_AB_matching_recall_late,1),'k--')
 p2 = errorbar(nanmean(TC_corr_AB_matching_recall_late(:,2:end),1),TC_recall_late_sem(2:end),'Color', [139,0,139]/255,'LineWidth',2)
-legend([p1 p2], {'Reference','Session'},'Location','southeast')
+legend([p1 p2], {'Day 1','Session'},'Location','southeast')
 xticks([1:6])
 xtickangle(45)
+
 for ii=2:7
     x_labels_learn{ii-1} = [ num2str(ii)];
 end
@@ -397,6 +446,7 @@ set(gca,'LineWidth',2)
 xlim([0,7])
 
 %% Plot - later STCs
+if 0
 figure('Position',[2002 44  364  945])
 subplot(2,2,1)
 imagesc(STC_AB_sort_A)
@@ -413,7 +463,7 @@ hold on
 title('Later day')
 subplot(2,2,4)
 imagesc(STC_AB_later_sort_B)
-
+end
 
 %% Merge D1 STCS for learning - sample matching neurons for visualization
 if 0
