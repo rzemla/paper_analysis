c53697466366bcdfc0b4df84ff76e199e4cd3e0b
Roland <rzemla@gmail.com>
2019-11-10 18:24:47 -0500

    updated lick histogram code for display in Figure 1 B

--- lick_histograms_adapted.m
+++ lick_histograms_adapted.m
@@ -4,24 +4,24 @@
 %list directories
 
 %I45 LT (1,1,1,1)
-distList{1} = {'G:\Figure_1_OCGOL_learning_long_term\I45_RT\behavior_only\I45_RT_rand_d1_052218',...
+dirList{1} = {'G:\Figure_1_OCGOL_learning_long_term\I45_RT\behavior_only\I45_RT_rand_d1_052218',...
             'G:\Figure_1_OCGOL_learning_long_term\I45_RT\behavior_only\I45_RT_5A5B_053018',...
             'G:\Figure_1_OCGOL_learning_long_term\I45_RT\behavior_only\I45_RT_3A3B_060518',...
             'G:\Figure_1_OCGOL_learning_long_term\I45_RT\behavior_only\I45_RT_AB_061418'};
 %I46 (1,1,1,1)
-distList{2} = {'G:\Figure_1_OCGOL_learning_long_term\I46\behavior_only\I46_rand_d1_052918',...
+dirList{2} = {'G:\Figure_1_OCGOL_learning_long_term\I46\behavior_only\I46_rand_d1_052918',...
                 'G:\Figure_1_OCGOL_learning_long_term\I46\behavior_only\I46_5A5B_060118',...
                 'G:\Figure_1_OCGOL_learning_long_term\I46\behavior_only\I46_3A3B_060718',...
                 'G:\Figure_1_OCGOL_learning_long_term\I46\behavior_only\I46_AB_061518'};
             
 %I47 RS (1,0,1,1)
-distList{3} = {'G:\Figure_1_OCGOL_learning_long_term\I47_RS\behavior_only\I47_RS_rand_d2_051518',...
+dirList{3} = {'G:\Figure_1_OCGOL_learning_long_term\I47_RS\behavior_only\I47_RS_rand_d2_051518',...
                 'G:\Figure_1_OCGOL_learning_long_term\I47_RS\behavior_only\I47_RS_5AB_d7_052218',...
                 'G:\Figure_1_OCGOL_learning_long_term\I47_RS\behavior_only\I47_RS_3AB_d8_052418',...
                 'G:\Figure_1_OCGOL_learning_long_term\I47_RS\behavior_only\I47_RS_AB_061418'};
             
 %I47LP (1,1,1,1)
-distList{4} = {'G:\Figure_1_OCGOL_learning_long_term\I47_LP\behavior_only\I47_LP_rand_d2_051518',...
+dirList{4} = {'G:\Figure_1_OCGOL_learning_long_term\I47_LP\behavior_only\I47_LP_rand_d2_051518',...
                 'G:\Figure_1_OCGOL_learning_long_term\I47_LP\behavior_only\I47_LP_5AB_d1_051718',...
                 'G:\Figure_1_OCGOL_learning_long_term\I47_LP\behavior_only\I47_LP_3AB_d8_052418',...
                 'G:\Figure_1_OCGOL_learning_long_term\I47_LP\behavior_only\I47_LP_AB_061418'};
@@ -29,13 +29,66 @@ distList{4} = {'G:\Figure_1_OCGOL_learning_long_term\I47_LP\behavior_only\I47_LP
 
 %% Load in Behavior struct with lick data
 
-for ii = 1:size(dirlist,2)
-   stages{ii} = load(fullfile()[dirlist{ii} '\Behavior.mat'],'licks','Behavior');    
+%for each animal
+tic
+for aa=1:4
+    for ss = 1:4
+        lick_data{aa}{ss} = load(fullfile(dirList{aa}{ss},'output','Behavior.mat'),'Behavior');
+    end
+end
+toc;
+
+%% Take all lick positions and bin 
+
+
+figure
+hold on
+ylim([0 0.5])
+b= bar(sum(lick_data{1, 1}{1, 1}.Behavior.lick.bin_licks_lap,1)./...
+    sum(sum(lick_data{1, 1}{1, 1}.Behavior.lick.bin_licks_lap,1)),1,'hist')
+b.FaceColor = 'g'
+
+
+%% Normalize lap positions in lick struct to this matrix
+
+%get max pos in cm for each lap
+for aa=1:4
+    for ss=1:4
+        max_pos{aa}{ss} = lick_data{aa}{ss}.Behavior.position_lap(:,2);
+    end
+end
+%get normalized position of each lick on each lap
+for aa=1:4
+    %for each session
+    for ss=1:4
+        %for each lap
+        for ll=1:size(lick_data{aa}{ss}.Behavior.lick.lick_lap,2)
+            lick_lap_norm{aa}{ss}{ll} = lick_data{aa}{ss}.Behavior.lick.lick_lap{ll}.position./max_pos{aa}{ss}(ll);
+        end
+    end
 end
 
-%% iterate the sequence below
+histogram(cell2mat(lick_lap_norm{aa}{ss}'),0:0.025:1,'Normalization','probability')
 
-edges = (0:5:200);
+
+%% Split into A and B normalized lick positions
+
+for aa=1:4
+    %for each session (skip RF sessions)
+    for ss=2:4
+        %correct and incorrect A trials
+        A_idx = find(lick_data{aa}{ss}.Behavior.performance.trialOrder == 2 | lick_data{aa}{ss}.Behavior.performance.trialOrder ==20);
+        lick_lap_norm_A{aa}{ss} = lick_lap_norm{aa}{ss}(A_idx);
+
+        %correct and incorrect B trials
+        B_idx = find(lick_data{aa}{ss}.Behavior.performance.trialOrder == 3 | lick_data{aa}{ss}.Behavior.performance.trialOrder ==30);
+        lick_lap_norm_B{aa}{ss} = lick_lap_norm{aa}{ss}(B_idx);
+        
+    end
+end
+
+
+%% iterate the sequence below
 
 for ss = 1:size(dirlist,2)
 
@@ -56,7 +109,8 @@ for ss = 1:size(dirlist,2)
             end
             
         end
-        
+    
+        %if RF session
     elseif ss == 1
         stages{ss}.lickCombined = [];
         for ii=1:size(stages{ss}.licks,2)
@@ -69,51 +123,58 @@ end
 
 
 %% Generate histograms
-magenta = [0.85, 0.42, 0.68];
-blue = [0.25,0.41,0.88];
-red =  [0.85,0.07,0.23];
+
+%which animal to generate plot from
+aa=2;
+%40 bins
+edges = (0:0.025:1);
+
+%magenta = [0.85, 0.42, 0.68];
+green = [50,205,50]./255;
+blue = [65,105,225]./255;
+red =  [220,20,60]./255;
 
 figure('Position', [100 100 600 800]);
 
 subplot(4,2,[1 2])
 hold on
 title('Random')
-plotHisto(stages{1}.lickCombined,edges,magenta);
+plotHisto(cell2mat(lick_lap_norm{aa}{1}'),edges,green);
 hold off
 
 subplot(4,2,3)
 hold on
 title('A')
-plotHisto(stages{2}.lickCombined{1},edges,blue);
+plotHisto(cell2mat(lick_lap_norm_A{aa}{2}'),edges,blue);
 hold off
 
 subplot(4,2,4)
 hold on
 title('B')
-plotHisto(stages{2}.lickCombined{2},edges,red);
+plotHisto(cell2mat(lick_lap_norm_B{aa}{2}'),edges,red);
 hold off
 
 subplot(4,2,5)
 hold on
 title('A')
-plotHisto(stages{3}.lickCombined{1},edges,blue);
+plotHisto(cell2mat(lick_lap_norm_A{aa}{3}'),edges,blue);
 hold off
 
 subplot(4,2,6)
 hold on
 title('B')
-plotHisto(stages{3}.lickCombined{2},edges,red);
+plotHisto(cell2mat(lick_lap_norm_B{aa}{3}'),edges,red);
 hold off
 
 subplot(4,2,7)
 hold on
 title('A')
-plotHisto(stages{4}.lickCombined{1},edges,blue);
+plotHisto(cell2mat(lick_lap_norm_A{aa}{4}'),edges,blue);
 hold off
 
 subplot(4,2,8)
 hold on
 title('B')
-plotHisto(stages{4}.lickCombined{2},edges,red);
+plotHisto(cell2mat(lick_lap_norm_B{aa}{4}'),edges,red);
 hold off
 
