8fca7b8531c71a339f506b6b5465f25cecff88b9
Roland <rzemla@gmail.com>
2021-03-04 23:21:03 -0500

    xtended stats export

--- stats/paired_wilcoxon_table_entry.m
+++ stats/paired_wilcoxon_table_entry.m
@@ -1,4 +1,4 @@
-function [t_out ] = paired_wilcoxon_table_entry_no_adj(num_in,sub_in,data_agg_in,...
+function [t_out ] = paired_wilcoxon_table_entry(num_in,sub_in,data_agg_in,...
                 comp_descrip, stats_in)
 
 %check if stats_in is a cell
@@ -21,19 +21,19 @@ data_agg = string(repmat(data_agg_in,nb_entries,1));
 %number of samples for each test            
 n_sample = stats_in_mat(:,3);
 %test ran
-test_name = string(repmat('1-sample Wilcoxon Sign Rank',nb_entries,1));
+test_name = string(repmat('Paired Wilcoxon Sign Rank',nb_entries,1));
 %degrees of freedom
 n_dof = stats_in_mat(:,4);
 %test statistic
 test_statistic = stats_in_mat(:,2);
-adj_method = string(repmat('N/A', nb_entries,1));
+adj_method =  string(repmat(['Holm-Sidak (',num2str(nb_entries),'-way)'], nb_entries,1));
 p = stats_in_mat(:,1);
-p_adj = string(repmat('N/A', nb_entries,1));
-sig_level = check_p_value_sig(p');
+p_adj =holm_sidak_p_adj(p',numel(p),0.05);
+sig_level = check_p_value_sig(p_adj');
 
-%create noRUN AUC/min table
+%table
 t_out = table(fig_num, fig_sub, data_agg, comp_descrip, n_sample,...
-            test_name, n_dof, test_statistic,p,p_adj, adj_method, sig_level,...
+            test_name, n_dof, test_statistic,p,p_adj', adj_method, sig_level,...
             'VariableNames',{'Figure','Subfigure','Data aggregation',...
             'Comparison','N', 'Test', 'Degrees of Freedom', 'Test statistic',...
             'p-value', 'p-value adjusted', 'Adjustment method','Significance'});
