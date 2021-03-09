89a4e36f9fee419df4da7b915c1befb6e57e0efa
Roland mobile <rzemla@gmail.com>
2021-03-09 14:45:25 -0500

    checkpoint

---
+++ stats/export_stats_word.m
@@ -0,0 +1,10 @@
+%% Import all table formatted entries here for data generated for each figure
+
+
+%% Make Word export stats script for each figure legend
+
+
+%% Word stats output 
+
+%modify this for extraction of formatted data to Word
+WriteToWordFromMatlab_testing
