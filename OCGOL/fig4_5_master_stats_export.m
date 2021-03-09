2e57410e3b50b03c697995fa18350c981f413b09
Roland <rzemla@gmail.com>
2021-03-08 21:54:44 -0500

    checkpoint

--- OCGOL/fig4_5_master_stats_export.m
+++ OCGOL/fig4_5_master_stats_export.m
@@ -5,10 +5,10 @@
 %import data for each figure
 
 %figure 2 source data
-%load('G:\Google_drive\task_selective_place_paper\matlab_data\source_data_fig4_5_and_sup.mat')
+load('G:\Google_drive\task_selective_place_paper\matlab_data\source_data_fig4_5_and_sup.mat')
 
 %laptop directory
-load('C:\Users\rzeml\Google Drive\task_selective_place_paper\matlab_data\source_data_fig4_5_and_sup.mat')
+%load('C:\Users\rzeml\Google Drive\task_selective_place_paper\matlab_data\source_data_fig4_5_and_sup.mat')
 
 
 %% Figure 4e Learning Analysis 
