85f65101ac7c2f071a8a919af6c97ced77419169
Roland mobile <rzemla@gmail.com>
2021-03-11 20:42:29 -0500

    done with figure 3 output stats to word

--- stats/formatted_stats_legend_output/export_stats_word_fig3.m
+++ stats/formatted_stats_legend_output/export_stats_word_fig3.m
@@ -179,7 +179,7 @@ txt_input = ')';
 writeDefaultWordText(ActXWord,WordHandle,txt_input);
 writeWordEnter(ActXWord,WordHandle,2);
 
-%% 3g - Global remapping neuron shift between reward zones 
+%% 3g - Global remapping neuron shift between reward zones - significant trends between zones
 
 %description of statistics
 txt_input = 'Fig 3g - Global remapping neurons switching fields between A and B laps between zones';
@@ -228,293 +228,26 @@ writePairedWilcoxAnimal_non_cor_pval(ActXWord,WordHandle,comp_descrip,test_stat,
 txt_input = '; ';
 writeDefaultWordText(ActXWord,WordHandle,txt_input);
 
+%AI to BII vs. AII to BI
+dof = table2array(fig3_data.table_list.t_frac_zone_remap(4,7));
+test_stat = table2array(fig3_data.table_list.t_frac_zone_remap(4,8));
+p_val = table2array(fig3_data.table_list.t_frac_zone_remap(4,10));
+p_val_non_cor = table2array(fig3_data.table_list.t_frac_zone_remap(4,9));
+sample_n = table2array(fig3_data.table_list.t_frac_zone_remap(4,5));
 
-
-
-%%
-%description of comparison
-comp_descrip = 'A-selective A vs. B lap activity rate';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
-
-%inset semicolon break
-txt_input = '; ';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%B-selective place cells (1)
-dof = table2array(fig2_data.table_list.t_auc_run(2,7));
-test_stat = table2array(fig2_data.table_list.t_auc_run(2,8));
-p_val = table2array(fig2_data.table_list.t_auc_run(2,10));
-sample_n = table2array(fig2_data.table_list.t_auc_run(2,5));
-
-%description of comparison
-comp_descrip = 'B-selective A vs. B lap activity rate';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
-
-%inset semicolon break
-txt_input = '; ';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%A&B-selective (non-selective) place cells (1)
-dof = table2array(fig2_data.table_list.t_auc_run(3,7));
-test_stat = table2array(fig2_data.table_list.t_auc_run(3,8));
-p_val = table2array(fig2_data.table_list.t_auc_run(3,10));
-sample_n = table2array(fig2_data.table_list.t_auc_run(3,5));
-
-%description of comparison
-comp_descrip = 'Non-selective A vs. B lap activity rate';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
-
-%close parenthesis and 2 newlines
-txt_input = ')';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-writeWordEnter(ActXWord,WordHandle,2);
-
-%% Figure 2c AUC/min comparison - NO RUN epochs
-
-%description of statistics
-txt_input = 'Fig 2c - AUC/min NO RUN epochs';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-writeWordEnter(ActXWord,WordHandle,1);
-
-%open parenthesis
-txt_input = '(';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%A-selective place cells (1) - adjusted p values here
-dof = table2array(fig2_data.table_list.t_auc_norun(1,7));
-test_stat = table2array(fig2_data.table_list.t_auc_norun(1,8));
-p_val = table2array(fig2_data.table_list.t_auc_norun(1,10));
-sample_n = table2array(fig2_data.table_list.t_auc_norun(1,5));
-
-%description of comparison
-comp_descrip = 'A-selective A vs. B lap activity rate';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
-
-%inset semicolon break
-txt_input = '; ';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%B-selective place cells (1)
-dof = table2array(fig2_data.table_list.t_auc_norun(2,7));
-test_stat = table2array(fig2_data.table_list.t_auc_norun(2,8));
-p_val = table2array(fig2_data.table_list.t_auc_norun(2,10));
-sample_n = table2array(fig2_data.table_list.t_auc_norun(2,5));
-
-%description of comparison
-comp_descrip = 'B-selective A vs. B lap activity rate';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
-
-%inset semicolon break
-txt_input = '; ';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%A&B-selective (non-selective) place cells (1)
-dof = table2array(fig2_data.table_list.t_auc_norun(3,7));
-test_stat = table2array(fig2_data.table_list.t_auc_norun(3,8));
-p_val = table2array(fig2_data.table_list.t_auc_norun(3,10));
-sample_n = table2array(fig2_data.table_list.t_auc_norun(3,5));
-
-%description of comparison
-comp_descrip = 'Non-selective A vs. B lap activity rate';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
-
-%close parenthesis and 2 newlines
-txt_input = ')';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-writeWordEnter(ActXWord,WordHandle,2);
-
-
-%% Figure 2d - Fraction of task-selective neurons by each tuning criterion
-
-%description of statistics
-txt_input = 'Fig 2d - SI/TS fraction tuned Friedman test';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-writeWordEnter(ActXWord,WordHandle,1);
-
-%open parenthesis
-txt_input = '(';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%Fig 2d - SI criterion Friedman (non-adjusted p vals (no comp)
-%get input data for Friedman test:
-dof = table2array(fig2_data.table_list.t_frac_si(1,7));
-test_stat = table2array(fig2_data.table_list.t_frac_si(1,8));
-p_val = table2array(fig2_data.table_list.t_frac_si(1,9));
-sample_n = table2array(fig2_data.table_list.t_frac_si(1,5));
-%description of comparison
-comp_descrip = 'S.I. criterion';
-%Fig. 2d friedman test write
-writeFriedmanTest(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
-
-%split entry with semicolor
-txt_input = '; ';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%Fig 2d - TS criterion Friedman
-%get input data for Friedman test:
-dof = table2array(fig2_data.table_list.t_frac_ts(1,7));
-test_stat = table2array(fig2_data.table_list.t_frac_ts(1,8));
-p_val = table2array(fig2_data.table_list.t_frac_ts(1,9));
-sample_n = table2array(fig2_data.table_list.t_frac_ts(1,5));
-%description of comparison
-comp_descrip = 'T.S. criterion';
-%Fig. 2d friedman test write
-writeFriedmanTest(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
-%close parenthesis
-txt_input = ')';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-writeWordEnter(ActXWord,WordHandle,2);
-
-%% Fig 2d A vs B comparison by SI and TS criterion
-
-%description of statistics
-txt_input = 'Fig 2d - A vs B by SI and TS criterion';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-writeWordEnter(ActXWord,WordHandle,1);
-
-%open parenthesis
-txt_input = '(';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%A vs B by SI criterion
-dof = table2array(fig2_data.table_list.t_frac_si(2,7));
-test_stat = table2array(fig2_data.table_list.t_frac_si(2,8));
-p_val = table2array(fig2_data.table_list.t_frac_si(2,10));
-sample_n = table2array(fig2_data.table_list.t_frac_si(2,5));
-
-%description of comparison
-comp_descrip = 'A vs. B - S.I. criterion';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n);
-
-%inset semicolon break
-txt_input = '; ';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%A vs B by TS criterion
-dof = table2array(fig2_data.table_list.t_frac_ts(2,7));
-test_stat = table2array(fig2_data.table_list.t_frac_ts(2,8));
-p_val = table2array(fig2_data.table_list.t_frac_ts(2,10));
-sample_n = table2array(fig2_data.table_list.t_frac_ts(2,5));
-
-%description of comparison
-comp_descrip = 'A vs. B - T.S. criterion';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n);
-
-%closed parenthesis
-txt_input = ')';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-writeWordEnter(ActXWord,WordHandle,2);
-
-%% Fig. 2d A vs A&B and B vs A&B for SI and TS criteria
-
-%description of statistics
-txt_input = 'Fig 2d - A vs A&B and B vs A&B by SI and TS criteria';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-writeWordEnter(ActXWord,WordHandle,1);
-
-%open parenthesis
-txt_input = '(';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%A vs A&BB by SI criterion
-dof = table2array(fig2_data.table_list.t_frac_si(3,7));
-test_stat = table2array(fig2_data.table_list.t_frac_si(3,8));
-p_val = table2array(fig2_data.table_list.t_frac_si(3,10));
-sample_n = table2array(fig2_data.table_list.t_frac_si(3,5));
-
-%description of comparison
-comp_descrip = 'A vs. A&B - S.I. criterion';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n);
-
-%inset semicolon break
-txt_input = '; ';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%B vs A&B by SI criterion
-dof = table2array(fig2_data.table_list.t_frac_si(4,7));
-test_stat = table2array(fig2_data.table_list.t_frac_si(4,8));
-p_val = table2array(fig2_data.table_list.t_frac_si(4,10));
-sample_n = table2array(fig2_data.table_list.t_frac_si(4,5));
-
-%description of comparison
-comp_descrip = 'B vs. A&B - S.I. criterion';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n);
-
-%inset semicolon break
-txt_input = '; ';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%A vs A&B by TS criterion
-dof = table2array(fig2_data.table_list.t_frac_ts(3,7));
-test_stat = table2array(fig2_data.table_list.t_frac_ts(3,8));
-p_val = table2array(fig2_data.table_list.t_frac_ts(3,10));
-sample_n = table2array(fig2_data.table_list.t_frac_ts(3,5));
-
-%description of comparison
-comp_descrip = 'A vs. A&B - T.S. criterion';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n);
-
-%inset semicolon break
-txt_input = '; ';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%B vs A&B by TS criterion
-dof = table2array(fig2_data.table_list.t_frac_ts(4,7));
-test_stat = table2array(fig2_data.table_list.t_frac_ts(4,8));
-p_val = table2array(fig2_data.table_list.t_frac_ts(4,10));
-sample_n = table2array(fig2_data.table_list.t_frac_ts(4,5));
-
-%description of comparison
-comp_descrip = 'B vs. A&B - T.S. criterion';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n);
-
-%closed parenthesis
-txt_input = ')';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-writeWordEnter(ActXWord,WordHandle,2);
-
-%% Figure 2f - A and B selective Rayleigh tests of circular uniformity - finish modifying this
-
-%description of statistics
-txt_input = 'Fig 2f - A and B selective tests of uniformity';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-writeWordEnter(ActXWord,WordHandle,1);
-
-%open parenthesis
-txt_input = '(';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-dof = table2array(fig2_data.table_list.t_pf_dist(1,7));
-test_stat = table2array(fig2_data.table_list.t_pf_dist(1,8));
-p_val = table2array(fig2_data.table_list.t_pf_dist(1,9));
-sample_n = table2array(fig2_data.table_list.t_pf_dist(1,5));
-%description of comparison
-comp_descrip = 'A-selective place field distribution';
-
-writeRayleighTest(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
-
-%inset semicolon break
-txt_input = '; ';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-dof = table2array(fig2_data.table_list.t_pf_dist(2,7));
-test_stat = table2array(fig2_data.table_list.t_pf_dist(2,8));
-p_val = table2array(fig2_data.table_list.t_pf_dist(2,9));
-sample_n = table2array(fig2_data.table_list.t_pf_dist(2,5));
-%description of comparison
-comp_descrip = 'B-selective place field distribution';
-
-writeRayleighTest(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
+%variant function that places non-corrected p val in parenthesis (fig 3)
+comp_descrip = 'AI to BII vs. AII to BI shift';
+writePairedWilcoxAnimal_non_cor_pval(ActXWord,WordHandle,comp_descrip,test_stat,p_val,p_val_non_cor, dof, sample_n)
 
 %close parenthesis
 txt_input = ')';
 writeDefaultWordText(ActXWord,WordHandle,txt_input);
 writeWordEnter(ActXWord,WordHandle,2);
 
-%% Figure 2g - A vs B place cell centroid difference - 2sample KS test
+%% Figure 3g - Non-significant cross zone shifts and intrazonal shifts stats
 
 %description of statistics
-txt_input = 'Fig 2g - A vs B place field centroid distribution';
+txt_input = 'Fig 3g - Global remapping neurons intra-zone shifts and nonsig cross zonal';
 writeDefaultWordText(ActXWord,WordHandle,txt_input);
 writeWordEnter(ActXWord,WordHandle,1);
 
@@ -522,13 +255,81 @@ writeWordEnter(ActXWord,WordHandle,1);
 txt_input = '(';
 writeDefaultWordText(ActXWord,WordHandle,txt_input);
 
-dof = table2array(fig2_data.table_list.t_2ks_pf_dist(1,7));
-test_stat = table2array(fig2_data.table_list.t_2ks_pf_dist(1,8));
-p_val = table2array(fig2_data.table_list.t_2ks_pf_dist(1,9));
-sample_n = table2array(fig2_data.table_list.t_2ks_pf_dist(1,5));
+%AI-BI vs. AII-BII
+dof = table2array(fig3_data.table_list.t_frac_zone_remap(1,7));
+test_stat = table2array(fig3_data.table_list.t_frac_zone_remap(1,8));
+p_val = table2array(fig3_data.table_list.t_frac_zone_remap(1,10));
+sample_n = table2array(fig3_data.table_list.t_frac_zone_remap(1,5));
 
 %description of comparison
-comp_descrip = 'A vs. B place field centroid difference';
+comp_descrip = 'AI-BI vs. AII-BII shift';
+writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
+
+%inset semicolon break
+txt_input = '; ';
+writeDefaultWordText(ActXWord,WordHandle,txt_input);
+
+%AI-BI vs. AIII-BIII
+dof = table2array(fig3_data.table_list.t_frac_zone_remap(2,7));
+test_stat = table2array(fig3_data.table_list.t_frac_zone_remap(2,8));
+p_val = table2array(fig3_data.table_list.t_frac_zone_remap(2,10));
+sample_n = table2array(fig3_data.table_list.t_frac_zone_remap(2,5));
+
+%description of comparison
+comp_descrip = 'AI-BI vs. AIII-BIII shift';
+writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
+
+%inset semicolon break
+txt_input = '; ';
+writeDefaultWordText(ActXWord,WordHandle,txt_input);
+
+%AII-BII vs. AIII-BIII
+dof = table2array(fig3_data.table_list.t_frac_zone_remap(3,7));
+test_stat = table2array(fig3_data.table_list.t_frac_zone_remap(3,8));
+p_val = table2array(fig3_data.table_list.t_frac_zone_remap(3,10));
+sample_n = table2array(fig3_data.table_list.t_frac_zone_remap(3,5));
+
+%description of comparison
+comp_descrip = 'AII-BII vs. AIII-BIII shift';
+writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
+
+%inset semicolon break
+txt_input = '; ';
+writeDefaultWordText(ActXWord,WordHandle,txt_input);
+
+%AI-BIII vs. AIII-BI
+dof = table2array(fig3_data.table_list.t_frac_zone_remap(6,7));
+test_stat = table2array(fig3_data.table_list.t_frac_zone_remap(6,8));
+p_val = table2array(fig3_data.table_list.t_frac_zone_remap(6,10));
+sample_n = table2array(fig3_data.table_list.t_frac_zone_remap(6,5));
+
+%description of comparison
+comp_descrip = 'AI-BIII vs. AIII-BI shift';
+writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
+
+%close parenthesis
+txt_input = ')';
+writeDefaultWordText(ActXWord,WordHandle,txt_input);
+writeWordEnter(ActXWord,WordHandle,2);
+
+%% Fig 3h - Distribution of partially remapping place field on A vs B trials
+
+%description of statistics
+txt_input = 'Fig 3h - distribution of partially remapping place fields on A vs. B trials';
+writeDefaultWordText(ActXWord,WordHandle,txt_input);
+writeWordEnter(ActXWord,WordHandle,1);
+
+%open parenthesis
+txt_input = '(';
+writeDefaultWordText(ActXWord,WordHandle,txt_input);
+
+dof = table2array(fig3_data.table_list.t_2ks_partial_pf_dist(1,7));
+test_stat = table2array(fig3_data.table_list.t_2ks_partial_pf_dist(1,8));
+p_val = table2array(fig3_data.table_list.t_2ks_partial_pf_dist(1,9));
+sample_n = table2array(fig3_data.table_list.t_2ks_partial_pf_dist(1,5));
+
+%description of comparison
+comp_descrip = 'A vs. B partial remapping neurons place field centroid difference';
 %modify this to run 2KS test
 write2ksTest(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
 
@@ -536,63 +337,6 @@ write2ksTest(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n)
 txt_input = ')';
 writeDefaultWordText(ActXWord,WordHandle,txt_input);
 writeWordEnter(ActXWord,WordHandle,2);
-
-%% Figure 2h - TC correlation between task selective/non selective place cells
-
-%description of statistics
-txt_input = 'Fig 2h - TC correlation between A vs B laps fo task selective/nonselective place cells';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-writeWordEnter(ActXWord,WordHandle,1);
-
-%open parenthesis
-txt_input = '(';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%A vs B sel
-sample_n = table2array(fig2_data.table_list.t_tc_corr(1,5));
-dof = table2array(fig2_data.table_list.t_tc_corr(1,7));
-test_stat = table2array(fig2_data.table_list.t_tc_corr(1,8));
-p_val = table2array(fig2_data.table_list.t_tc_corr(1,10));
-
-%description of comparison
-comp_descrip = 'A- vs. B- selective';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n);
-
-%inset semicolon break
-txt_input = '; ';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-%A vs AB
-sample_n = table2array(fig2_data.table_list.t_tc_corr(2,5));
-dof = table2array(fig2_data.table_list.t_tc_corr(2,7));
-test_stat = table2array(fig2_data.table_list.t_tc_corr(2,8));
-p_val = table2array(fig2_data.table_list.t_tc_corr(2,10));
-
-%description of comparison
-comp_descrip = 'A-selective vs. A&B';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n);
-
-%inset semicolon break
-txt_input = '; ';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-
-
-%B vs AB
-sample_n = table2array(fig2_data.table_list.t_tc_corr(3,5));
-dof = table2array(fig2_data.table_list.t_tc_corr(3,7));
-test_stat = table2array(fig2_data.table_list.t_tc_corr(3,8));
-p_val = table2array(fig2_data.table_list.t_tc_corr(3,10));
-
-%description of comparison
-comp_descrip = 'B-selective vs. A&B';
-writePairedWilcoxAnimal(ActXWord,WordHandle,comp_descrip,test_stat,p_val, dof, sample_n);
-
-%close parenthesis
-txt_input = ')';
-writeDefaultWordText(ActXWord,WordHandle,txt_input);
-writeWordEnter(ActXWord,WordHandle,2);
-
-%% Import and embed the number of neurons in the above stats (last)
 
 %% Close Word document
 CloseWord(ActXWord,WordHandle,FileSpec);
